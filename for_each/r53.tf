1b8918d29331ffa17addcf379ad70d4033e826f8
praveensri453 <praveensri92@gmail.com>
2024-11-03 15:27:12 +0530

    dynamic

---
+++ for_each/r53.tf
@@ -0,0 +1,11 @@
+
+resource "aws_route53_record" "expense" {
+  for_each = aws_instance.expense #created instance 3 o/ps
+  zone_id = var.zone_id
+  #backend.daws81s.online
+  name            = each.key == "frontend" ? var.domain_name : "${each.key}.${var.domain_name}"
+  type            = "A"
+  ttl             = 1
+  records         = each.key == "frontend" ? [each.value.public_ip] : [each.value.private_ip]
+  allow_overwrite = true
+}
